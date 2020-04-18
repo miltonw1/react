9d5e64a256fdd27dd106ccc000be88ca4da252c0
Milton Wery <milton_st@hotmail.com>
2020-04-18 16:29:33 -0300

    Aprendiendo a agregar estilos heredados e inline

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = ( props ) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>Soy {props.name} y tengo {props.age} años!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
