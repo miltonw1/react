e09a14ec67dc8af38a162b3d3e854eade969da35
Milton Wery <milton_st@hotmail.com>
2020-04-19 17:28:06 -0300

    Mostrando y ocultando elementos mediante operaciones ternarias

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,6 +35,11 @@ class App extends Component {
     } )
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render () {
     const style ={
       backgroundColor: 'white',
@@ -49,7 +55,10 @@ class App extends Component {
         <p>This is really working!</p>
         <button
         style={style} 
-        onClick={() => this.switchNameHandler('Milton!')}>Switch Name</button>
+        onClick={this.togglePersonsHandler}>Ocultar/Mostrar personas</button>
+        {
+        this.state.showPersons === true ?  
+          <div>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
@@ -61,6 +70,9 @@ class App extends Component {
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
+        </div> : null
+        }
+       
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
