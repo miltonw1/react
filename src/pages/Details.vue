77ccd15774094614e5f1cc53675709b23aa1d114
Milton Wery <milton_st@hotmail.com>
2021-01-13 20:00:43 -0300

    Vigilantes

---
+++ src/pages/Details.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page padding class="bg-white">
+    <div class="q-pa-md bg">
+      <div class="row">
+         <div class="offset-lg-2 offset-md-3 offset-sm-2 col-lg-8 col-md-6 col-sm-8 col-12 bg-white">
+            <div class="row">
+            <div class="col-5 q-pa-sm">
+                <q-img src="guerin-logo.png" />
+            </div>
+            </div>
+            <div class="text-center title q-pa-sm">Detalle del puesto principal</div>
+            <div class="row q-pd">
+              <q-separator color="red" inset class="full-width" />
+            </div>
+          </div>
+        </div>
+      <div class="row q-pa-md">
+        <q-btn-dropdown @click="setDiagramacion" no-caps color="primary" label="Diagramacion" class="full-width q-ma-sm">
+        </q-btn-dropdown>
+        <div v-if="renderOption.diagramacion">Diagramacion</div>
+        <q-btn-dropdown @click="setAlertas" no-caps color="primary" label="Alertas" class="full-width q-ma-sm">
+        </q-btn-dropdown>
+        <div v-if="renderOption.alertas">Alertas</div>
+         <q-btn-dropdown @click="setHombre" no-caps color="primary" label="Hombre Vivo" class="full-width q-ma-sm">
+        </q-btn-dropdown>
+        <div v-if="renderOption.hombre">Hombre Vivo</div>
+        <q-btn-dropdown @click="setHombre" no-caps color="grey" label="Control de supervisión" class="full-width q-ma-sm">
+        </q-btn-dropdown>
+        <div v-if="renderOption.supervision">Control</div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      renderOption: {
+        diagramacion: false,
+        alertas: false,
+        hombre: false,
+        supervision: false
+      }
+    }
+  },
+
+  methods: {
+    setDiagramacion () {
+      this.renderOption.diagramacion = true
+      this.renderOption.alertas = false
+      this.renderOption.hombre = false
+      this.renderOption.supervision = false
+    },
+    setAlertas () {
+      this.renderOption.diagramacion = false
+      this.renderOption.alertas = true
+      this.renderOption.hombre = false
+      this.renderOption.supervision = false
+    },
+    setHombre () {
+      this.renderOption.diagramacion = false
+      this.renderOption.alertas = false
+      this.renderOption.hombre = true
+      this.renderOption.supervision = false
+    },
+    setSupervision () {
+      this.renderOption.diagramacion = false
+      this.renderOption.alertas = false
+      this.renderOption.hombre = false
+      this.renderOption.supervision = true
+    }
+  }
+
+}
+</script>
---
+++ src/pages/Vigilantes.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-page padding class="bg-white">
+    <div class="q-pa-md bg-white">
+      <div class="row">
+         <div class="offset-lg-2 offset-md-3 offset-sm-2 col-lg-8 col-md-6 col-sm-8 col-12 bg-white">
+        <div class="row items-center">
+            <div class="col">
+                <q-img src="guerin-logo.png" />
+            </div>
+            <div class="col text-center">
+                <q-img src="no-avatar.png" />
+                Nombre Supervisor
+            </div>
+        </div>
+            <q-input rounded class="q-pa-sm" v-model="search" filled type="search" label="Buscar objetivos">
+                <template v-slot:append>
+                <q-icon name="search" />
+                </template>
+            </q-input>
+              <div class="row">
+              <q-separator color="dark" inset class="full-width" />
+              </div>
+              <div class="row justify-around items-center">
+                <div class="col-2">
+                <q-img src="supervisor.png"/>
+                <div style="font-size: 0.7rem" class="text-center">Jose Vigilador</div>
+                </div>
+                <div class="col-6">
+                    <div style="font-size: 0.9rem">Nombre del puesto</div>
+                    <div style="font-size: 0.8">Cliente </div>
+                    <div style="font-size: 0.7rem">Calle, numero y ciudad</div>
+                    <div class="lastcontrol" style="font-size: 0.7rem">Ultimo control: 10/10/2020</div>
+                </div>
+                <div class="col-0.5"><div class="schedule">10:00</div><div class="schedule">18:00</div></div>
+                <div class="col-1 text-green">
+                 <q-icon size="sm" name="call"/>
+                </div>
+                <div class="col-1 text-primary">
+                 <q-icon size="sm" name="reorder" />
+                </div>
+            </div>
+            <div class="row">
+              <q-separator color="dark" inset class="full-width" />
+            </div>
+            <div class="row justify-around items-center">
+                <div class="col-2">
+                <q-img src="supervisor.png"/>
+                <div style="font-size: 0.7rem" class="text-center">Jose Vigilador</div>
+                </div>
+                <div class="col-6">
+                    <div style="font-size: 0.9rem">Nombre del puesto</div>
+                    <div style="font-size: 0.8">Cliente </div>
+                    <div style="font-size: 0.7rem">Calle, numero y ciudad</div>
+                    <div class="lastcontrol" style="font-size: 0.7rem">Ultimo control: 10/10/2020</div>
+                </div>
+                <div class="col-0.5"><div class="schedule">10:00</div><div class="schedule">18:00</div></div>
+                <div class="col-1 text-green">
+                 <q-icon size="sm" name="call"/>
+                </div>
+                <div class="col-1 text-primary">
+                 <q-icon size="sm" name="reorder" />
+                </div>
+            </div>
+            <div class="row">
+              <q-separator color="dark" inset class="full-width" />
+            </div>
+            <div class="row justify-around items-center">
+                <div class="col-2">
+                <q-img src="supervisor.png"/>
+                <div style="font-size: 0.7rem" class="text-center">Jose Vigilador</div>
+                </div>
+                <div class="col-6">
+                    <div style="font-size: 0.9rem">Nombre del puesto</div>
+                    <div style="font-size: 0.8">Cliente </div>
+                    <div style="font-size: 0.7rem">Calle, numero y ciudad</div>
+                    <div class="lastcontrol" style="font-size: 0.7rem">Ultimo control: 10/10/2020</div>
+                </div>
+                <div class="col-0.5"><div class="schedule">10:00</div><div class="schedule">18:00</div></div>
+                <div class="col-1 text-green">
+                 <q-icon size="sm" name="call"/>
+                </div>
+                <div class="col-1 text-primary">
+                 <q-icon size="sm" name="reorder" />
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </q-page>
+</template>
